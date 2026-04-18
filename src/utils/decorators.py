5a67441dac424906ecb192ce3d412be5a7c747a7
Christian Schinkel <107631710+ChristianSchinkel@users.noreply.github.com>
2026-04-18 16:29:50 +0200

    Add execution time measurement to get_time decorator

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -5,6 +5,45 @@
 #             the base function. Pass the base function
 #             as an argument to the decorator:
 
+from functools import wraps
+from typing import Callable, Any
+import time
+
+
+def get_time(func: Callable) -> Callable:
+    """ Decorator that measures the execution time of a function.
+    """
+    # Preserve the original function's metadata …
+    @wraps(func)  # …(name, docstring, etc.)
+    def wrapper(*args, **kwargs) -> Any:
+        """ Wrapper function that measures the execution time
+        of the original function.
+        Args:
+            *args: Positional arguments for the original function.
+            **kwargs: Keyword arguments for the original function.
+        Returns:
+            Any: The result of the original function.
+        """
+        start_time: float = time.perf_counter()
+        result: Any = func(*args, **kwargs)
+        end_time: float = time.perf_counter()
+        print(f"Ran {func.__name__} in {end_time - start_time:.2f} seconds")
+
+        return result
+
+    return wrapper
+
+
+@get_time
+def expensive_function() -> None:
+    """
+    A function that simulates an expensive operation.
+    Returns:
+        None
+    """
+    time.sleep(2)  # Simulate an expensive operation
+    print("Done!")
+
 
 def add_sprinkles(func):
     """ Decorator that adds sprinkles to a cake.
@@ -40,6 +79,12 @@ def main():
     """ Main function.
     """
     make_cake("chocolate")
+
+    # Measure the execution time of the expensive function
+    print(expensive_function.__name__)
+    print(expensive_function.__doc__)
+    print(expensive_function.__annotations__)
+    expensive_function()
 
 
 if __name__ == "__main__":
